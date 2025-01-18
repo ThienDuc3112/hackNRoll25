035deaf7a72548511da42277be770fd2920af590
Nguyen Thien Duc <ntduc4@gmail.com>
2025-01-18 12:13:46 +0800

    Init branch

--- frontend/src/pages/editor/section.tsx
+++ frontend/src/pages/editor/section.tsx
@@ -4,47 +4,12 @@ import { Component } from "./component";
 import { useAtomValue } from "jotai";
 import { itemMapAtom } from "./state";
 
-//type SectionProp = {
-//  id: UniqueIdentifier;
-//  title: string;
-//  children?: ReactNode
-//};
-//
-//export const Section = (props: SectionProp) => {
-//  const { isOver, setNodeRef } = useDroppable({
-//    id: props.id
-//  });
-//  return (
-//    <div
-//      ref={setNodeRef}
-//      style={{
-//        minWidth: 50,
-//        padding: 8,
-//        width: "100%",
-//      }}
-//    >
-//      <div style={{
-//        border: "dashed 1px",
-//        display: "flex",
-//        flexDirection: "column",
-//        minHeight: "200px",
-//        backgroundColor: isOver ? 'green' : 'grey',
-//      }}>
-//        <span style={{ color: "white", textAlign: "center" }}>{props.title}</span>
-//        <div style={{ width: "100%", height: "100%", margin: 0 }}>
-//          {props.children}
-//        </div>
-//      </div>
-//    </div>
-//  );
-//};
-
 type SectionProp = {
-  section: SectionType
+  section: SectionType;
 };
 
 export const Section = (props: SectionProp) => {
-  const itemMap = useAtomValue(itemMapAtom)
+  const itemMap = useAtomValue(itemMapAtom);
   return (
     <SortableContext items={props.section.items} id={props.section.id}>
       <div
@@ -54,21 +19,31 @@ export const Section = (props: SectionProp) => {
           width: "100%",
         }}
       >
-        <div style={{
-          border: "dashed 1px",
-          display: "flex",
-          flexDirection: "column",
-          minHeight: "200px",
-        }}>
-          <span style={{ color: "black", textAlign: "center", flexGrow: 0 }}>{props.section.displayName}</span>
-          <div style={{ width: "100%", height: "100%", flexGrow: 1, margin: 0 }}>
+        <div
+          style={{
+            border: "dashed 1px",
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "200px",
+          }}
+        >
+          <span style={{ color: "black", textAlign: "center", flexGrow: 0 }}>
+            {props.section.displayName}
+          </span>
+          <div
+            style={{ width: "100%", height: "100%", flexGrow: 1, margin: 0 }}
+          >
             {props.section.items.map((item, i) => (
-              <Component item={itemMap[item]} key={i} data={{
-                sortable: {
-                  containerId: props.section.id,
-                  index: i
-                }
-              }} />
+              <Component
+                item={itemMap[item]}
+                key={i}
+                data={{
+                  sortable: {
+                    containerId: props.section.id,
+                    index: i,
+                  },
+                }}
+              />
             ))}
           </div>
         </div>
@@ -76,4 +51,3 @@ export const Section = (props: SectionProp) => {
     </SortableContext>
   );
 };
-
