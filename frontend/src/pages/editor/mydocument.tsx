ec5a8aba4ffe3b4723bbebebd08057b2b7e318e2
Trung Bui <thetrung205@gmail.com>
2025-01-19 10:04:14 +0800

    Style CSS for PDF

--- frontend/src/pages/editor/mydocument.tsx
+++ frontend/src/pages/editor/mydocument.tsx
@@ -1,56 +1,89 @@
 import React from "react";
 import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
 import { UserMetaDataItem, SectionType } from "./types";
+import { itemMapAtom } from "./state";
+import { useAtomValue } from "jotai";
 
 const styles = StyleSheet.create({
   page: {
-    padding: 30,
-    fontFamily: "Helvetica",
+    padding: "1in",
+    fontFamily: "Times-Roman",
   },
   header: {
     marginBottom: 20,
     textAlign: "center",
   },
   name: {
-    fontSize: 24,
-    marginBottom: 10,
+    fontSize: 20,
+    marginBottom: 8,
     fontWeight: "bold",
+    textTransform: "uppercase",
+    fontFamily: "Times-Bold",
   },
   contacts: {
     flexDirection: "row",
     justifyContent: "center",
-    fontSize: 10,
-    gap: 10,
+    fontSize: 11,
+    gap: 8,
   },
   divider: {
-    margin: "0 5px",
+    margin: "0 4",
   },
   section: {
-    marginBottom: 20,
+    marginBottom: 12,
   },
   sectionTitle: {
-    fontSize: 16,
-    marginBottom: 10,
+    fontSize: 12,
     fontWeight: "bold",
-    color: "#333",
-    backgroundColor: "#f5f5f5",
-    padding: "5 10",
-    borderRadius: 3,
+    textTransform: "uppercase",
+    borderBottomWidth: 1,
+    borderBottomColor: "#000",
+    borderBottomStyle: "solid",
+    paddingBottom: 3,
+    marginBottom: 8,
+    fontFamily: "Times-Bold",
   },
   field: {
     marginBottom: 8,
   },
   fieldLabel: {
-    fontSize: 12,
-    marginBottom: 4,
+    fontSize: 11,
     fontWeight: "bold",
-    color: "#444",
+    marginBottom: 2,
+    fontFamily: "Times-Bold",
   },
   fieldContent: {
-    fontSize: 10,
-    color: "#666",
+    fontSize: 11,
+    color: "#000",
     lineHeight: 1.4,
   },
+  pointData: {
+    flexDirection: "row",
+    paddingLeft: 12,
+    marginBottom: 4,
+  },
+  bullet: {
+    marginRight: 6,
+    fontSize: 11,
+  },
+  pointContent: {
+    flex: 1,
+    fontSize: 11,
+    lineHeight: 1.4,
+  },
+  sectionTitle: {
+    fontSize: 11,
+    fontWeight: "bold",
+    fontFamily: "Times-Bold",
+  },
+  sectionTime: {
+    fontSize: 11,
+    fontStyle: "italic",
+  },
+  sectionDes: {
+    fontSize: 11,
+    lineHeight: 1.4,
+  }
 });
 
 const Mydocument = ({
@@ -61,38 +94,60 @@ const Mydocument = ({
   name: string;
   metadatas: UserMetaDataItem[];
   sections: SectionType[];
-}) => (
-  <Document>
-    <Page size="A4" style={styles.page}>
-      {/* Contact */}
-      <View style={styles.header}>
-        <Text style={styles.name}>{name}</Text>
-        <View style={styles.contacts}>
-          {metadatas.map((field, index) => (
-            <React.Fragment key={field.id}>
-              <Text>{field.value}</Text>
-              {index < metadatas.length - 1 && (
-                <Text style={styles.divider}>•</Text>
-              )}
-            </React.Fragment>
-          ))}
+}) => {
+  const itemMap = useAtomValue(itemMapAtom);
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        {/* Contact */}
+        <View style={styles.header}>
+          <Text style={styles.name}>{name}</Text>
+          <View style={styles.contacts}>
+            {metadatas.map((field, index) => (
+              <React.Fragment key={field.id}>
+                <Text>{field.value}</Text>
+                {index < metadatas.length - 1 && (
+                  <Text style={styles.divider}>-</Text>
+                )}
+              </React.Fragment>
+            ))}
+          </View>
         </View>
-      </View>
 
-      {/* Sections */}
-      {sections.map((section) => (
-        <View key={section.id} style={styles.section}>
-          <Text style={styles.sectionTitle}>{section.displayName}</Text>
-          {sections.map((field) => (
-            <View key={field.id} style={styles.field}>
-              {/* <Text style={styles.fieldLabel}>{field.label}</Text>
-              <Text style={styles.fieldContent}>{field.content}</Text> */}
-            </View>
-          ))}
-        </View>
-      ))}
-    </Page>
-  </Document>
-);
+        {/* Sections */}
+        {sections.map((section) => (
+          <View key={section.id} style={styles.section}>
+            <Text style={styles.sectionTitle}>{section.displayName}</Text>
+            {sections.map((section) => (
+              <View key={section.id} style={styles.field}>
+                {section.items.map((itemId) => {
+                  const item = itemMap[itemId];
+                  if (item.type === "POINT") {
+                    return (
+                      <View style={styles.pointData}>
+                        <Text style={styles.bullet}>•</Text>
+                        <Text style={styles.pointContent}>{item.data}</Text>
+                      </View>
+                    );
+                  } else {
+                    return (
+                      <View style={styles.field}>
+                        <View style={{ flexDirection: "row", justifyContent: "space-between", marginBottom: 2 }}>
+                          <Text style={styles.sectionTitle}>{item.title}</Text>
+                          <Text style={styles.sectionTime}>{item.timeRange}</Text>
+                        </View>
+                        <Text style={styles.sectionDes}>{item.description}</Text>
+                      </View>
+                    );
+                  }
+                })}
+              </View>
+            ))}
+          </View>
+        ))}
+      </Page>
+    </Document>
+  );
+};
 
 export default Mydocument;
