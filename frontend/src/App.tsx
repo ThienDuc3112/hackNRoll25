eada547ae6325e04ea06c96651619f68f4e8ef1a
Nguyen Thien Duc <ntduc4@gmail.com>
2025-01-19 10:57:57 +0800

    Add routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import { Routes, Route } from "react-router";
 import Editor from "./pages/editor/editor";
+import LandingPage from "./pages/editor/landingpage";
 
 function App() {
-  return <Editor />;
+  return <Routes>
+    <Route index element={<LandingPage />} />
+    <Route path="/editor" element={<Editor />} />
+  </Routes>;
 }
 
 export default App;
